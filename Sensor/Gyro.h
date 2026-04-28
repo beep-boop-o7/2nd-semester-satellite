7db29e36e63e94da19c16b716eb9e8d18d6d2c8b
Pimpsten <flinds24@student.aau.dk>
2026-04-28 14:56:11 +0200

    Update Gyro.h

--- Sensor/Gyro.h
+++ Sensor/Gyro.h
@@ -1,1 +1,22 @@
+#include <SPI.h>
+#include <math.h>
 
+struct {
+  float Angle = 0;
+  float Filtered = 0.0;
+  float offset = 0.0;
+  float Sensitivity = 0.0048;
+  unsigned long LastTime = 0;
+} Gyro_var;
+
+void GyroMeasurementMode();
+
+void GyroVerifyConnection();
+
+int16_t readGyroX();
+
+void calibrateGyro(int samples);
+
+void GyroInit(int samples);
+
+void updateGyro();
