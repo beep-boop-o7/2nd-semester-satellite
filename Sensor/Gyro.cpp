28b021003dd4ef19c6db94fd78b9e6cdff41786b
beep-boop-o7 <andreas@lorenzen.us>
2026-04-29 12:54:08 +0200

    Update Gyro.cpp

--- Sensor/Gyro.cpp
+++ Sensor/Gyro.cpp
@@ -1,78 +1,61 @@
-#define CS_PIN 8
+#include "Gyro.h"
 
-// Globale variabler
-struct {
-  float Angle = 0;
-  float Filtered = 0.0;
-  float offset = 0.0;
-  float Sensitivity = 0.0048;
-  unsigned long LastTime = 0;
-} Gyro_var;
-
-void GyroMeasurementMode() {
+void Gyro_Measurement_Mode(int CS_pin) {
   // Enable measurement mode
-  digitalWrite(CS_PIN, LOW);
-  SPI.transfer(0x10);
-  SPI.transfer(0x02);
-  digitalWrite(CS_PIN, HIGH);
+  SPI_Write_16(CS_pin, 3, 0x10, 0x02);
 
-  delayMicroseconds(50);
+  k_sleep(1);
 }
 
-void GyroVerifyConnection() {
+void Gyro_Verify_Connection(int CS_pin) {
   // Check ID
-  digitalWrite(CS_PIN, LOW);
-  SPI.transfer(0x80);
-  uint8_t id = SPI.transfer(0x00);
-  digitalWrite(CS_PIN, HIGH);
+  uint8_t id = 0;
+  SPI_Read_16(CS_pin, 3, 0x00, &id);
 
   Serial.print("ID (173 forventet): ");
   Serial.println(id);
-  delayMicroseconds(50);
+  k_sleep(1);
 
   // Verify
-  digitalWrite(CS_PIN, LOW);
-  SPI.transfer(0x90);
-  uint8_t val = SPI.transfer(0x00);
-  digitalWrite(CS_PIN, HIGH);
+  uint8_t val = 0;
+  SPI_Read_16(CS_pin, 3, 0x10, &val);
 
   Serial.print("POWER_CTL (2 forventet): ");
   Serial.println(val);
-  delayMicroseconds(50);
+  k_sleep(1);
 }
 
-int16_t readGyroY() {
-  digitalWrite(CS_PIN, LOW);
-  SPI.transfer(0x8A);
+int16_t Read_Gyro_Y(int CS_pin) {
   uint8_t low = SPI.transfer(0x00);
   uint8_t high = SPI.transfer(0x00);
-  digitalWrite(CS_PIN, HIGH);
+  SPI_Read_16(CS_pin, 3, 0x0A, &low);
+  SPI_Read_16(CS_pin, 3, 0x0B, &high);
 
   return (high << 8) | low;
 }
 
-void calibrateGyro(int samples) {
+void Calibrate_Gyro(int samples, int CS_pin) {
   Serial.println("Kalibrerer... hold sensoren stille");
   long sum = 0;
   for (int i = 0; i < samples; i++) {
-    int16_t raw = readGyroY();
+    int16_t raw = Read_Gyro_Y(CS_pin);
 
     sum += raw;
-    delay(5);
+    k_sleep(5);
   }
   Gyro_var.offset = (float)sum / samples;
   Serial.print("Offset: ");
   Serial.println(Gyro_var.offset);
 }
 
-void GyroInit(int samples) {
-  GyroVerifyConnection();
-  GyroMeasurementMode();
-  calibrateGyro(samples);
+void Gyro_Init(int samples, int CS_pin) {
+  Gyro_Verify_Connection(CS_pin);
+  Gyro_Measurement_Mode(CS_pin);
+  Calibrate_Gyro(samples, CS_pin);
 }
 
-void updateGyro() {
-  int32_t raw = readGyroY();
+void Update_Gyro(int CS_pin) {
+  int32_t raw = Read_Gyro_Y(CS_pin);
 
   unsigned long now = micros();
   float dt = (now - Gyro_var.LastTime) * 1e-6f;
@@ -90,10 +73,9 @@ void updateGyro() {
   Serial.print(Gyro_var.Angle, 1);
   Serial.print(" til tiden: ");
   Serial.println(millis());
-  delay(10);
 }
 
-void gyroSetup(){
-  GyroInit(1000);
+void Gyro_Setup(int CS_pin){
+  Gyro_Init(1000, CS_pin);
   Gyro_var.LastTime = micros();
 }
