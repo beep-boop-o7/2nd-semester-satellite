64648e792358ccad3e39d5bb462576edd4dabafa
beep-boop-o7 <andreas@lorenzen.us>
2026-04-29 14:27:51 +0200

    Update V2.1_kode_sensor.h

--- Sensor/V2_2_kode_sensor_cpp/V2.1_kode_sensor.h
+++ Sensor/V2_2_kode_sensor_cpp/V2.1_kode_sensor.h
@@ -1,15 +1,26 @@
-#ifndef Hall_effect_sensor
-#define Hall_effect_sensor
+#ifndef Hall_effect_TMAG
+#define Hall_effect_TMAG
 
-#include "TMAG5170.hpp"
+#include "../Integration/rt_spi.h"
 #include <krnl.h>
 
+typedef struct { //data for each axis
+	float x, y, z;
+} hall_sensor_data;
 
-void taskSensor(float *Bx,float *By,float *Bz);
 
-void callUpSemHallEffect(void);
+/**
+ * @brief read x, y, z from TMAG5170
+ * @param data struct that data will be returned in
+ * @param CS_pin the pin chip select is connected too
+ * @param alert_pin the pin alert is connected to for alert signaled conversion
+ */
+int Read_TMAG(hall_sensor_data *data, int CS_pin, int alert_pin);
 
-void startUpTMAG();
+/**
+ * @brief initilize TMAG5170
+ * @param CS_pin the pin chip select is connected too
+ */
+void Init_TMAG(int CS_pin);
 
-int magnetorquerSem;
 #endif
