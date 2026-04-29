44bc429d95b376de63de76dcbfdab0716e796940
beep-boop-o7 <andreas@lorenzen.us>
2026-04-29 13:08:07 +0200

    Update Gyro.h

--- Sensor/Gyro.h
+++ Sensor/Gyro.h
@@ -1,6 +1,6 @@
 #include "../Integration/rt_spi.h"
 #include <math.h>
-#define CS_PIN 8
+#include <krnl.h>
 
 struct {
   float Angle = 0.0;
@@ -10,20 +10,46 @@ struct {
   unsigned long LastTime = 0;
 } Gyro_var;
 
-void GyroMeasurementMode();
-
-void GyroVerifyConnection();
-
-int16_t readGyroY();
-
-void calibrateGyro(int samples);
-
-void GyroInit(int samples);
-
-void updateGyro();
-
+/**
+ * @brief set measurement mode
+ * @param CS_pin the pin chip select is connected too
+ */
+void Gyro_Measurement_Mode(int CS_pin);
 
 /**
- * @brief Sætter gyroskopet op, indeholder GyroInit()
+ * @brief verify connection is established
+ * @param CS_pin the pin chip select is connected too
  */
-void gyroSetup(); 
+void Gyro_Verify_Connection(int CS_pin);
+
+/**
+ * @brief read y-axis of gyroscope
+ * @param CS_pin the pin chip select is connected too
+ */
+int16_t Read_GyroY(int CS_pin);
+
+/**
+ * @brief calabrate gyroscope
+ * @param samples num of samples used to calabrate gyroscope
+ * @param CS_pin the pin chip select is connected too
+ */
+void Calibrate_Gyro(int samples, int CS_pin);
+
+/**
+ * @brief initializes gyroscope
+ * @param samples num of samples used to calabrate gyroscope
+ * @param CS_pin the pin chip select is connected too
+ */
+void Gyro_Init(int samples, int CS_pin);
+
+/**
+ * @brief updates gyroscope (needs clarification)
+ * @param CS_pin the pin chip select is connected too
+ */
+void Update_Gyro(int CS_pin);
+
+/**
+ * @brief Starter gyroskopet, og indeholder GyroInit()
+ * @param CS_pin the pin chip select is connected too
+ */
+void Gyro_Setup(int CS_pin); 
