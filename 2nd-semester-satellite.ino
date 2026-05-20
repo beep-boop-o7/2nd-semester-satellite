c453a87cd3e2d0ec1f953046bfad6e37d085d527
Rasmus <rasmusbkragh@gmail.com>
2026-05-20 16:13:23 +0200

    new1

--- 2nd-semester-satellite.ino
+++ 2nd-semester-satellite.ino
@@ -26,237 +26,242 @@
 #define MULTIPLEX_B 2
 #define SENS_A A6
 #define SENS_B A7
-#define MAXIMUM_MILLIAMPRE 150 //for now
+#define MAXIMUM_MILLIAMPRE 200  //for now
 
-#define STK 150
+#define STK 300
 char t1[STK], t2[STK], t3[STK], t4[STK], t5[STK];
 
 struct k_t *pTask_Hall, *pTask_Gyro, *pTask_Control, *pTask_Mag, *pTask_C2;
 
 struct k_t *Timed_Sem1, *Timed_Sem2, *Timed_Sem3, *Magnetic_Sem;
 
-hall_sensor_data Hall_Data_Queue[4];
+hall_sensor_data Hall_Data_Queue[2];
 
-gyro_sensor_data Gyro_Data_Queue[4];
+gyro_sensor_data Gyro_Data_Queue[2];
 
-control_data Mag_Data_Queue[4];
+control_data Mag_Data_Queue[2];
 
-control_data C2_Data_Queue[4];
+control_data C2_Data_Queue[2];
 
 struct k_msg_t *Queue_Hall, *Queue_Gyro, *Queue_Mag, *Queue_C2;
 
 void Task_Hall() {
-	//setup hall effect sensor
-    hall_sensor_data hall_data;
+  //setup hall effect sensor
+  hall_sensor_data hall_data;
 
-    k_set_sem_timer(Timed_Sem1, 100);
+  k_set_sem_timer(Timed_Sem1, 100);
 
-    pinMode(HALL_CS_PIN, OUTPUT);
-    pinMode(HALL_ALERT_PIN, OUTPUT);
-	digitalWrite(HALL_CS_PIN, HIGH);
-    digitalWrite(HALL_ALERT_PIN, HIGH);
+  pinMode(HALL_CS_PIN, OUTPUT);
+  pinMode(HALL_ALERT_PIN, OUTPUT);
+  digitalWrite(HALL_CS_PIN, HIGH);
+  digitalWrite(HALL_ALERT_PIN, HIGH);
 
-    Init_TMAG(HALL_CS_PIN);
+  Init_TMAG(HALL_CS_PIN);
 
-    while (1) {
-		//loop hall effect sensor
-        k_wait(Timed_Sem1, 0);   //read every 100ms
-        k_wait(Magnetic_Sem, 0); //mutex with magnetorquer
+  while (1) {
+    //loop hall effect sensor
+    k_wait(Timed_Sem1, 0);  //read every 100ms
+    k_wait(Magnetic_Sem, 0);  //mutex with magnetorquer
+    
+    Read_TMAG(&hall_data, HALL_CS_PIN, HALL_ALERT_PIN);
 
-        Read_TMAG(&hall_data, HALL_CS_PIN, HALL_ALERT_PIN);
-
-        k_signal(Magnetic_Sem);
-
-        k_send(Queue_Hall, &hall_data);
-    }
+    k_signal(Magnetic_Sem);
+    
+    k_send(Queue_Hall, &hall_data);
+  }
 }
 
 void Task_Gyro() {
-	//setup gyroscope
-    gyro_sensor_data gyro_data;
+  //setup gyroscope
+  gyro_sensor_data gyro_data;
 
-    k_set_sem_timer(Timed_Sem2, 20);
+  k_set_sem_timer(Timed_Sem2, 20);
 
-    pinMode(GYRO_CS_PIN, OUTPUT);
-    digitalWrite(GYRO_CS_PIN, HIGH);
+  pinMode(GYRO_CS_PIN, OUTPUT);
+  digitalWrite(GYRO_CS_PIN, HIGH);
 
-    Gyro_Setup(GYRO_CS_PIN);
+  Gyro_Setup(GYRO_CS_PIN);
 
-    while (1) {
-		//loop gyroscope
-        k_wait(Timed_Sem2, 0);
+  while (1) {
+    //loop gyroscope
+    k_wait(Timed_Sem2, 0);
 
-        Update_Gyro(&gyro_data, GYRO_CS_PIN);
+    Update_Gyro(&gyro_data, GYRO_CS_PIN);
 
-        k_send(Queue_Gyro, &gyro_data);
-    }
+    k_send(Queue_Gyro, &gyro_data);
+  }
 }
 
 void Task_Control() {
-	//setup control
-    hall_sensor_data hall_tmp; //temp buckets for message queue
-    gyro_sensor_data gyro_tmp;
-    control_data gyro_data, hall_data, command_direction, output_data, C2_tmp;
-    int missed;
-	
-    hall_data.x = 0;
-    hall_data.y = 0;
-    hall_data.z = 0;
+  //setup control
+  hall_sensor_data hall_tmp;  //temp buckets for message queue
+  gyro_sensor_data gyro_tmp;
+  control_data gyro_data, hall_data, command_direction, output_data, C2_tmp;
+  int missed;
 
-    gyro_data.x = 0;
-    gyro_data.y = 0;
-    gyro_data.z = 0;
+  hall_data.x = 1;
+  hall_data.y = 0;
+  hall_data.z = 0;
 
-    command_direction.x = 0;
-    command_direction.y = 0;
-    command_direction.z = 0;
+  gyro_data.x = 1;
+  gyro_data.y = 0;
+  gyro_data.z = 0;
 
-    double z_angle = 0;
+  command_direction.x = 1;
+  command_direction.y = 0;
+  command_direction.z = 0;
 
-    k_set_sem_timer(Timed_Sem3, 1000);
+  double z_angle = 0;
 
-    while (1) {
-		//loop control
-        if(k_receive(Queue_C2, &C2_tmp, -1, &missed) != -1) {
-            command_direction = C2_tmp;
-        }
+  k_set_sem_timer(Timed_Sem3, 1000);
 
-        if(k_receive(Queue_Hall, &hall_tmp, -1, &missed) != -1) {
-            hall_data.x = hall_tmp.x;
-            hall_data.y = hall_tmp.y;
-            hall_data.z = hall_tmp.z;
-        }
-
-        if(k_receive(Queue_Gyro, &gyro_tmp, -1, &missed) != -1) {
-            //if 3 axis gyro copy twice
-            gyro_data.z = gyro_tmp.rate;
-            float z_rotation = atan2f(command_direction.y, command_direction.x); //update direction
-            float z_new_rotation = z_rotation + (gyro_tmp.Angle - z_angle);
-            float z_scale = (sqrt(sq(command_direction.x) + sq(command_direction.y)) / sqrt(sq(sin(z_new_rotation)) + sq(cos(z_new_rotation))));
-            command_direction.x = sin(z_new_rotation) * z_scale;
-            command_direction.y = cos(z_new_rotation) * z_scale;
-        }
-
-        if(k_wait(Timed_Sem3, -1) > 0) {
-            Control_Magnetic(command_direction, gyro_data, hall_data, &output_data);
-            k_send(Queue_Mag, &output_data);
-            Serial.print("rate:");
-            Serial.println(gyro_data.z);
-            Serial.print("angle:");
-            Serial.println(gyro_tmp.Angle);
-        }
-
-		k_sleep(1);
+  while (1) {
+    //loop control
+    if (k_receive(Queue_C2, &C2_tmp, -1, &missed) != -1) {
+      command_direction = C2_tmp;
     }
+
+    if (k_receive(Queue_Hall, &hall_tmp, -1, &missed) != -1) {
+      hall_data.x = hall_tmp.x;
+      hall_data.y = hall_tmp.y;
+      hall_data.z = hall_tmp.z;
+    }
+
+    if (k_receive(Queue_Gyro, &gyro_tmp, -1, &missed) != -1) {
+      //if 3 axis gyro copy twice
+      gyro_data.z = gyro_tmp.rate;
+      float z_rotation = atan2f(command_direction.y, command_direction.x);  //update direction
+      float z_new_rotation = z_rotation + (gyro_tmp.Angle - z_angle);
+      float z_scale = (sqrt(sq(command_direction.x) + sq(command_direction.y)) / sqrt(sq(sin(z_new_rotation)) + sq(cos(z_new_rotation))));
+      command_direction.x = sin(z_new_rotation) * z_scale;
+      command_direction.y = cos(z_new_rotation) * z_scale;
+    }
+
+    if (k_wait(Timed_Sem3, -1) > 0) {
+      Control_Magnetic(command_direction, gyro_data, hall_data, &output_data);
+      Serial.print("values:");
+      Serial.println(output_data.x );
+      k_send(Queue_Mag, &output_data);
+      k_sleep(1);
+      Serial.print(F("rate:"));
+      Serial.println(gyro_data.z);
+      Serial.print(F("angle:"));
+      Serial.println(gyro_tmp.Angle);
+    }
+
+    k_sleep(1);
+  }
 }
 
 void Task_Mag() {
-	//setup magnetorquer
-    control_data tmp_cmd;
-    int missed;
-    Init_Magnetorquers(X_AXIS_A, X_AXIS_B, X_AXIS_PWM, Y_AXIS_A, Y_AXIS_B, Y_AXIS_PWM, Z_AXIS_A, Z_AXIS_B, Z_AXIS_PWM, MULTIPLEX_A, MULTIPLEX_B, SENS_A, SENS_B, MAXIMUM_MILLIAMPRE);
-    while (1) {
-		//loop magnetorquer
-        if (k_receive(Queue_Mag, &tmp_cmd, -1, &missed) != -1) { //if command receved
-            Serial.println("msg");
-            k_wait(Magnetic_Sem, 0); //mutex with hall effect sensor
-            Serial.println("sem");
-            aktuator_data data;
-            data.x = tmp_cmd.x;
-            data.y = tmp_cmd.y;
-            data.z = 0.0;
-            long int run_time = millis();
-            Command_Magnetorquers(data);
+  //setup magnetorquer
+  control_data tmp_cmd;
+  int missed;
+  Init_Magnetorquers(X_AXIS_A, X_AXIS_B, X_AXIS_PWM, Y_AXIS_A, Y_AXIS_B, Y_AXIS_PWM, Z_AXIS_A, Z_AXIS_B, Z_AXIS_PWM, MULTIPLEX_A, MULTIPLEX_B, SENS_A, SENS_B, MAXIMUM_MILLIAMPRE);
+  while (1) {
+    Serial.println("in mag");
+    //loop magnetorquer
+    k_receive(Queue_Mag, &tmp_cmd, 0, &missed);  //if command receved
+   
+    k_wait(Magnetic_Sem, 0);  //mutex with hall effect sensor
+    
+    aktuator_data data;
+    data.x = tmp_cmd.x;
+    data.y = tmp_cmd.y;
+    data.z = 0.0;
+    long int run_time = millis();
+    Command_Magnetorquers(data);
 
-            k_sleep(run_time - millis() + 800); //80% run time
+    k_sleep(run_time - millis() + 800);  //80% run time
 
-            Stop_Magnetorquers();
-            k_signal(Magnetic_Sem);
-        }
-        k_sleep(5);
-    }
+   Stop_Magnetorquers();
+    k_signal(Magnetic_Sem);
+
+    Serial.println(F("magIn"));
+  }
 }
 
 control_data Convert_Msg(char *in_buffer) {
-    control_data command;
-    command.x = 0.0;
-    command.y = 0.0;
-    command.z = 0.0;
-    char *buffer; //<data|data|data>
-	buffer = strsep(&in_buffer, "<");
-    buffer = strsep(&in_buffer, "|");
-    if(buffer == nullptr) return command;
-    command.x = atof(buffer);
-    buffer = strsep(&in_buffer, "|");
-    if(buffer == nullptr) return command;
-    command.y = atof(buffer);
-    buffer = strsep(&in_buffer, "|");
-    if(buffer == nullptr) return command;
-    command.z = atof(buffer);
-    return command;
+  control_data command;
+  command.x = 0.0;
+  command.y = 0.0;
+  command.z = 0.0;
+  char *buffer;  //<data|data|data>
+  buffer = strsep(&in_buffer, "<");
+  buffer = strsep(&in_buffer, "|");
+  if (buffer == nullptr) return command;
+  command.x = atof(buffer);
+  buffer = strsep(&in_buffer, "|");
+  if (buffer == nullptr) return command;
+  command.y = atof(buffer);
+  buffer = strsep(&in_buffer, "|");
+  if (buffer == nullptr) return command;
+  command.z = atof(buffer);
+  return command;
 }
 
 void Task_C2() {
-    control_data command;
-    char in_buffer[40];
-    uint8_t read_pos = 0;
+  control_data command;
+  char in_buffer[40];
+  uint8_t read_pos = 0;
 
-    Serial.println("ready");
+  Serial.println(F("ready"));
 
-    while (1) {
-        if(Serial.available()) {
-            char in_byte = Serial.read();
-            if(in_byte == '<') {
-                read_pos = 0;
-            }
+  while (1) {
+    if (Serial.available()) {
+      char in_byte = Serial.read();
+      if (in_byte == '<') {
+        read_pos = 0;
+      }
 
-            if(in_byte == '>') {
-                in_buffer[read_pos] = '\0';
-                command = Convert_Msg(in_buffer);
+      if (in_byte == '>') {
+        in_buffer[read_pos] = '\0';
+        command = Convert_Msg(in_buffer);
 
-                k_send(Queue_C2, &command);
-            }
+        k_send(Queue_C2, &command);
+      }
 
-            in_buffer[read_pos] = in_byte;
-            read_pos++;
-        }
+      in_buffer[read_pos] = in_byte;
+      read_pos++;
     }
+  }
 }
 
 void setup() {
-	Serial.begin(9600);
-    while (!Serial);    
-	
-	Serial.println("Serial");
-	
-	k_init(5, 5, 4);
-	//     |  |  |-- num of msg Queues (4) hall effect, gyroscope, magnatorquer, C2
-	//     |  |----- num of semaphores (5) SPI, timer 1-2-3, magnetic lockout
-	//     |------------- num of tasks (5) hall, gyro, control, mag, C2
-	
-	Queue_Hall = k_crt_send_Q(4, sizeof(hall_sensor_data),  Hall_Data_Queue);  // 4 elements of size
-	Queue_Gyro = k_crt_send_Q(4, sizeof(gyro_sensor_data),  Gyro_Data_Queue);  // 4 elements of size
-    Queue_Mag = k_crt_send_Q(4, sizeof(control_data),  Mag_Data_Queue);  // 4 elements of size
-    Queue_C2 = k_crt_send_Q(4, sizeof(control_data),  C2_Data_Queue);  // 4 elements of size
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 
-	pTask_Hall = k_crt_task(Task_Hall, 10, t1, STK);
-	pTask_Gyro = k_crt_task(Task_Gyro, 10 , t2, STK);
-    pTask_Control = k_crt_task(Task_Control, 20, t3, STK);
-    pTask_Mag = k_crt_task(Task_Mag, 15, t4, STK);
-    pTask_C2 = k_crt_task(Task_C2, 30, t5, STK);
+  Serial.println(F("Serial"));
 
-	Timed_Sem1 = k_crt_sem(0, 1); // 0: start value, 1: max value (clipping)
-	Timed_Sem2 = k_crt_sem(0, 1); // 0: start value, 1: max value (clipping)
-    Timed_Sem3 = k_crt_sem(0, 1); // 0: start value, 1: max value (clipping)
-    Magnetic_Sem = k_crt_sem(0, 1); // 0: start value, 1: max value (clipping)
+  k_init(5, 5, 4);
+  //     |  |  |-- num of msg Queues (4) hall effect, gyroscope, magnatorquer, C2
+  //     |  |----- num of semaphores (5) SPI, timer 1-2-3, magnetic lockout
+  //     |------------- num of tasks (5) hall, gyro, control, mag, C2
 
-    Init_SPI(SPI_MOSI, SPI_MISO, SPI_CLOCK);
+  Queue_Hall = k_crt_send_Q(2, sizeof(hall_sensor_data), Hall_Data_Queue);  // 4 elements of size
+  Queue_Gyro = k_crt_send_Q(2, sizeof(gyro_sensor_data), Gyro_Data_Queue);  // 4 elements of size
+  Queue_Mag = k_crt_send_Q(2, sizeof(control_data), Mag_Data_Queue);        // 4 elements of size
+  Queue_C2 = k_crt_send_Q(2, sizeof(control_data), C2_Data_Queue);          // 4 elements of size
 
-	Serial.println("k_start");
-	
-	k_start(); /* start krnl timer speed 1 milliseconds*/
-	
-	Serial.println("If you see this then krnl crashed :-(");
+  pTask_Hall = k_crt_task(Task_Hall, 10, t1, STK);
+  pTask_Gyro = k_crt_task(Task_Gyro, 10, t2, STK);
+  pTask_Control = k_crt_task(Task_Control, 20, t3, STK);
+  pTask_Mag = k_crt_task(Task_Mag, 15, t4, STK);
+  pTask_C2 = k_crt_task(Task_C2, 30, t5, STK);
+
+  Timed_Sem1 = k_crt_sem(0, 1);    // 0: start value, 1: max value (clipping)
+  Timed_Sem2 = k_crt_sem(0, 1);    // 0: start value, 1: max value (clipping)
+  Timed_Sem3 = k_crt_sem(0, 1);    // 0: start value, 1: max value (clipping)
+  Magnetic_Sem = k_crt_sem(1, 1);  // 0: start value, 1: max value (clipping)
+
+  Init_SPI(SPI_MOSI, SPI_MISO, SPI_CLOCK);
+
+  Serial.println(F("k_start"));
+
+  k_start(); /* start krnl timer speed 1 milliseconds*/
+
+  Serial.println(F("If you see this then krnl crashed :-("));
 }
 
 void loop() {}
--- src/Aktuator/Aktuator.ino
+++ src/Aktuator/Aktuator.ino
@@ -1,36 +1,40 @@
 #include "Aktuator.h"
 
 void setup() {
-    Init_Magnetorquers(12, 11, 10, 8, 7, 9, 5, 4, 6, A5, 2, A6, A7, 200.0);
+    Init_Magnetorquers(12, 11, 10, 8, 7, 9, 5, 4, 6, 5, 2, A6, A7, 200.0);
     Serial.begin(9600);
     while (!Serial);
 }
 
 void loop() {
-    aktuator_data data;
+       aktuator_data data;
     data.x = 1.0;
     data.y = 0.0;
     data.z = 0.0;
     Command_Magnetorquers(data);
-    delay(500);
+    delay(2000);
     Stop_Magnetorquers();
-    delay(500);
+    delay(100);
     data.x = 0.0;
     data.y = 1.0;
     Command_Magnetorquers(data);
-    delay(500);
+    delay(2000);
     Stop_Magnetorquers();
-    delay(500);
+    delay(100);
     data.x = -1.0;
     data.y = 0.0;
     Command_Magnetorquers(data);
-    delay(500);
+    delay(2000);
     Stop_Magnetorquers();
-    delay(500);
+    delay(100);
     data.x = 0.0;
     data.y = -1.0;
     Command_Magnetorquers(data);
-    delay(500);
+    delay(2000);
     Stop_Magnetorquers();
+
     delay(500);
 }
+
+
+
